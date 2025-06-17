0a8120fabc49c05de85a6e0ef5b9eba4ead772d6
Sagnik Roy Chowdhury <sroychowdhury317@gmail.com>
2025-06-17 17:33:55 +0530

    Update dsc2.java

--- dsc2.java
+++ dsc2.java
@@ -1,3 +1,10 @@
+//2. Remove duplicates from array
+ /* Write a function that inputs an array. This function should return an array that has the
+ elements in the same order, but with each element appearing only once. Assume the
+ input array is already sorted.
+ Example Test Case:
+ Input: [2,3,4,4,6,7,7]
+ Output: [2,3,4,6,7] */
 import java.util.*;
 class dsc2
 {
@@ -13,20 +20,15 @@ class dsc2
         {
             ar[i] = sc.next().charAt(0);
         }
-        System.out.print("INPUT ARRAY ---> [ ");
-        for(i=0;i<n;i++)
-        System.out.print(ar[i]+" ");
-        System.out.print("]");
-        System.out.println();
         for(i=0;i<n-1;i++)
         {
             if(ar[i] == ar[i+1])
                 ar[i]='#';
         }  
-        System.out.print("OUTPUT ARRAY ---> [ ");
+        System.out.print("OUTPUT: [ ");
         for(i=0;i<n;i++)
-            if(ar[i] != '#')
-                System.out.print(ar[i]+" ");
+        if(ar[i] != '#')
+        System.out.print(ar[i]+" ");
         System.out.print("]");
     }
 }
