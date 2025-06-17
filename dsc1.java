28bd7cb0c58265af46795ed8e5a626891f242e9a
Sagnik Roy Chowdhury <sroychowdhury317@gmail.com>
2025-06-17 17:26:44 +0530

    Update dsc1.java

--- dsc1.java
+++ dsc1.java
@@ -1,3 +1,9 @@
+//1. Arrange words in a string in the order of their length
+ /*Write a function that inputs a string. The function should return the string sorted in
+ ascending order of the length of the words.
+ Example Test Case:
+ Input: "This is a cool sentence"
+ Output: "a is this cool sentence" */
 import java.util.*;
 class dsc1
 {
